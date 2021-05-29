7fe35eadded34de82a286cb38c894971d836ff31
Steven <lean0016@algonquinlive.com>
2021-05-29 17:42:00 -0400

    Finished Phone Version of Lab 3

--- StevensAndroidLabs/app/src/main/java/algonquin/cst2335/lean0016/MainActivity.java
+++ StevensAndroidLabs/app/src/main/java/algonquin/cst2335/lean0016/MainActivity.java
@@ -4,20 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.View;
+
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.widget.EditText;
-import android.widget.ImageButton;
+
 import android.widget.ImageView;
 import android.widget.Switch;
-import android.widget.RadioButton;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.content.Context;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,56 +22,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button mybtn = findViewById(R.id.button);
-        TextView mytext = findViewById(R.id.textview);
-        EditText myedit = findViewById(R.id.myedittext);
-        CheckBox checkBox1 = findViewById(R.id.mycheckbox_yes);
-        Switch myswitch1 = findViewById(R.id.myswitch1);
-        RadioButton radioButton1 = findViewById(R.id.radiobutton1);
 
-        mybtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String editString = myedit.getText().toString();
-                mytext.setText("Your edit text has: " + editString);
-            }
-        });
+        ImageView imgView;
+        Switch sw;
+            setContentView(R.layout.activity_main);
 
-            checkBox1.setOnCheckedChangeListener((btn, isChecked) -> {
-                CharSequence Text = "You clicked the Checkbox and it is now:" + isChecked;
-                int duration = Toast.LENGTH_LONG;
-                Toast toast = Toast.makeText(this, Text, duration);
-                toast.show();
+            imgView = findViewById(R.id.flagview);
+            sw = findViewById(R.id.spin_switch);
+
+            sw.setOnCheckedChangeListener( (btn, isChecked) -> {
+                if (isChecked)
+                {
+                    RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                    rotate.setDuration(5000);
+                    rotate.setRepeatCount(Animation.INFINITE);
+                    rotate.setInterpolator(new LinearInterpolator());
+
+                    imgView.startAnimation(rotate);
+                }
+                else {
+                    imgView.clearAnimation();
+                }
             });
-
-
-             myswitch1.setOnCheckedChangeListener((btn, isChecked) -> {
-                String Text = "You clicked the Switch and it is now:" + isChecked;
-                Toast toast = Toast.makeText(this, Text, Toast.LENGTH_SHORT);
-                toast.show();
-        });
-
-            radioButton1.setOnCheckedChangeListener((btn, isChecked) -> {
-                String Text = "You clicked the RadioButton and it is now:" + isChecked;
-                Toast toast = Toast.makeText(this, Text, Toast.LENGTH_SHORT);
-                toast.show();
-            });
-
-        ImageButton imgbtn = (ImageButton) findViewById(R.id.myimagebutton);
-        ImageView myimage = findViewById(R.id.myimg);
-        imgbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int width = myimage.getWidth();
-                int height = myimage.getHeight();
-                Toast.makeText(getApplicationContext(), "The width = " + width + "and height = " + height, Toast.LENGTH_LONG).show();
-
-            }
-        });
+        }
 
     }
 
 
-    }
+
 
 
 

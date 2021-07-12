73c0ed77ef45103b8882feca44d8138ab0225b05
Steven <lean0016@algonquinlive.com>
2021-07-12 10:47:12 -0400

    working on fragments

---
+++ StevensAndroidLabs/app/src/main/java/algonquin/cst2335/lean0016/MessageListFragment.java
@@ -0,0 +1,282 @@
+package algonquin.cst2335.lean0016;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
+public class MessageListFragment extends Fragment {
+
+    MyChatAdapter adapter;
+    ArrayList<ChatMessage> messages = new ArrayList<>();
+    RecyclerView chatList;
+    SQLiteDatabase db;
+    Cursor results;
+    Button send;
+
+
+    public MessageListFragment()
+    {
+        int i = 0;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View chatLayout = inflater.inflate(R.layout.chatlayout, container, false);
+
+        EditText typedMessage = chatLayout.findViewById(R.id.editMessage);
+        send = chatLayout.findViewById(R.id.sendbutton);
+        Button rec = chatLayout.findViewById(R.id.receiveButton);
+        chatList = chatLayout.findViewById(R.id.myrecycler);
+        MyOpenHelper opener = new MyOpenHelper(getContext());
+        db = opener.getWritableDatabase();
+
+        results = db.rawQuery("Select * from " + MyOpenHelper.TABLE_NAME + ";", null);
+
+        int _idCol = results.getColumnIndex("_id");
+        int messageCol = results.getColumnIndex(MyOpenHelper.col_message);
+        int sendCol = results.getColumnIndex(MyOpenHelper.col_send_recieve);
+        int timeCol = results.getColumnIndex(MyOpenHelper.col_time_sent);
+
+        while (results.moveToNext()) {
+            long id = results.getInt(_idCol);
+            String message = results.getString(messageCol);
+            String time = results.getString(timeCol);
+            int sendOrReceive = results.getInt(sendCol);
+            messages.add(new ChatMessage(message, sendOrReceive, time, id));
+        }
+        adapter = new MyChatAdapter();
+        chatList.setAdapter(adapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        chatList.setLayoutManager(layoutManager);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("EEEE, dd-MMM-yyyy hh-mm-ss a", Locale.getDefault());
+
+
+        send.setOnClickListener(click -> {
+            String currentDataandTime = sdf.format(new Date());
+            ChatMessage thisMessage = new ChatMessage(typedMessage.getText().toString(), 1, currentDataandTime);
+
+            ContentValues newRow = new ContentValues();
+            newRow.put(MyOpenHelper.col_message, thisMessage.getMessage());
+            newRow.put(MyOpenHelper.col_send_recieve, thisMessage.getSendOrReceive());
+            newRow.put(MyOpenHelper.col_time_sent, thisMessage.getTimeSent());
+            db.insert(MyOpenHelper.TABLE_NAME, MyOpenHelper.col_message, newRow);
+
+            messages.add(thisMessage);
+            //clear the edittext:
+            typedMessage.setText("");
+            //refresh the list:
+            adapter.notifyItemInserted(messages.size() - 1); //just insert the new row:
+
+        });
+
+        rec.setOnClickListener(click -> {
+            String currentDataandTime = sdf.format(new Date());
+            ChatMessage thisMessage = new ChatMessage(typedMessage.getText().toString(), 2, currentDataandTime);
+
+            ContentValues newRow = new ContentValues();
+            newRow.put(MyOpenHelper.col_message, thisMessage.getMessage());
+            newRow.put(MyOpenHelper.col_send_recieve, thisMessage.getSendOrReceive());
+            newRow.put(MyOpenHelper.col_time_sent, thisMessage.getTimeSent());
+            db.insert(MyOpenHelper.TABLE_NAME, MyOpenHelper.col_message, newRow);
+
+            messages.add(thisMessage);
+            //clear the edittext:
+            typedMessage.setText("");
+            //refresh the list:
+            adapter.notifyItemInserted(messages.size() - 1); //just insert the new row:
+
+        });
+
+        return chatLayout;
+
+
+    }
+
+    public void notifyMessageDeleted(ChatMessage chosenMessage, int chosenPosition) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setMessage("Are you sure want to delete the message: " + chosenMessage.getMessage())
+                .setTitle("Danger!")
+                .setNegativeButton("Cancel", (dialog, cl) -> {
+                })
+                .setPositiveButton("Delete", (dialog, cl) -> {
+
+                    //position = getAbsoluteAdapterPosition();
+
+                    ChatMessage removedMessage = messages.get(chosenPosition);
+                    messages.remove(chosenPosition);
+                    adapter.notifyItemRemoved(chosenPosition);
+
+                    db.delete(MyOpenHelper.TABLE_NAME, "_id=?", new String[]{
+                            Long.toString(removedMessage.getId())
+                    });
+
+                    Snackbar.make(send, "You deleted message #" + chosenPosition, Snackbar.LENGTH_SHORT)
+                            .setAction("UNDO", clk -> {
+                                messages.add(chosenPosition, removedMessage);
+                                adapter.notifyItemInserted(chosenPosition);
+
+                                db.execSQL(" Insert into " + MyOpenHelper.TABLE_NAME + " values('" + removedMessage.getId() +
+                                        "','" + removedMessage.getMessage() +
+                                        "','" + removedMessage.getSendOrReceive() +
+                                        "','" + removedMessage.getTimeSent() + "');");
+                            })
+                            .show();
+                })
+                .create().show();
+    }
+
+    private class MyRowViews extends RecyclerView.ViewHolder {
+
+        TextView messageText;
+        TextView timetext;
+        int position = -1;
+
+        public MyRowViews(View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(click -> {
+                ChatRoom parentActivity = (ChatRoom) getContext();
+                int position = getAbsoluteAdapterPosition();
+                parentActivity.userClickedMessage(messages.get(position), position);
+                /*
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setMessage("Do you want to delete the message: " + messageText.getText())
+                        .setTitle("Question:")
+                        .setNegativeButton("No", (dialog, cl) -> {
+                        })
+                        .setPositiveButton("Yes", (dialog, cl) -> {
+
+                            position = getAbsoluteAdapterPosition();
+
+                            ChatMessage removedMessage = messages.get(position);
+                            messages.remove(position);
+                            adapter.notifyItemRemoved(position);
+
+                            db.delete(MyOpenHelper.TABLE_NAME, "_id=?", new String[]{
+                                    Long.toString(removedMessage.getId())
+                            });
+
+                            Snackbar.make(messageText, "You deleted message #" + position, Snackbar.LENGTH_LONG)
+                                    .setAction("Undo", clk -> {
+                                        messages.add(position, removedMessage);
+                                        adapter.notifyItemInserted(position);
+
+                                        db.execSQL(" Insert into " + MyOpenHelper.TABLE_NAME + " values('" + removedMessage.getId() +
+                                                "','" + removedMessage.getMessage() +
+                                                "','" + removedMessage.getSendOrReceive() +
+                                                "','" + removedMessage.getTimeSent() + "');");
+                                    })
+                                    .show();
+                        })
+                        .create().show();*/
+            });
+
+            messageText = itemView.findViewById(R.id.message);
+            timetext = itemView.findViewById(R.id.time);
+        }
+
+
+        public void setPosition(int p) {
+            position = p;
+        }
+
+    }
+
+    class MyChatAdapter extends RecyclerView.Adapter {
+        @Override
+        public int getItemViewType(int position) {
+            ChatMessage thisRow = messages.get(position);
+            return thisRow.getSendOrReceive();
+        }
+
+        @Override
+        public MyRowViews onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = getLayoutInflater();
+            int layoutID;
+            if (viewType == 1)
+                layoutID = R.layout.sent_message;
+            else
+                layoutID = R.layout.receive_message;
+
+            View loadedRow = inflater.inflate(layoutID, parent, false);
+
+            return new MyRowViews(loadedRow);
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            MyRowViews RowLayout = (MyRowViews) holder;
+            RowLayout.messageText.setText(messages.get(position).getMessage());
+            RowLayout.timetext.setText(messages.get(position).getTimeSent());
+            RowLayout.setPosition(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return messages.size();
+        }
+    }
+
+
+    static class ChatMessage {
+        public String messages;
+        int sendOrReceive;
+        String timeSent;
+        long id;
+
+        public void setId(long l) {
+            id = l;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+        public ChatMessage(String messages, int sendOrReceive, String timeSent) {
+            this.messages = messages;
+            this.sendOrReceive = sendOrReceive;
+            this.timeSent = timeSent;
+
+        }
+
+        public ChatMessage(String messages, int sendOrReceive, String timeSent, long id) {
+            this.messages = messages;
+            this.sendOrReceive = sendOrReceive;
+            this.timeSent = timeSent;
+            setId(id);
+        }
+
+        public String getMessage() {
+            return messages;
+        }
+
+        public int getSendOrReceive() {
+            return sendOrReceive;
+        }
+
+        public String getTimeSent() {
+            return timeSent;
+        }
+    }
+}
